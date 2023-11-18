c09aea77f40cb6a7ea4d1ef37b89589a3fc0066b
philibertEtienne <philibert.etienne4@orange.fr>
2023-11-18 14:59:29 +0100

    test

---
+++ app/profile/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <>
+    <div>profile</div>
+    <div>profile</div>
+    <div>profile</div>
+    <div>profile</div>
+    <div>profile</div>
+    <div>profile</div>
+    <div>profile</div>
+    <div>profile</div>
+    <div>profile</div>
+    <div>profile</div>
+    <div>profile</div>
+    <div>profile</div>
+    <div>profile</div>
+    <div>profile</div>
+    <div>profile</div>
+    </>
+    )
+}
